18b4482c91847aba41e0d4d28c066f048362eb7c
weeger <romain.weeger@gmail.com>
2025-04-02 08:32:53 +0200

    Add json

--- src/Helper/FileHelper.php
+++ src/Helper/FileHelper.php
@@ -11,13 +11,14 @@ class FileHelper
     final public const EXTENSION_SEPARATOR = '.';
 
     final public const FILE_EXTENSION_CSV = 'csv';
+    final public const FILE_EXTENSION_JSON = 'json';
     final public const FILE_EXTENSION_PDF = 'pdf';
     final public const FILE_EXTENSION_PHP = 'php';
-    final public const FILE_EXTENSION_YML = 'yml';
-    final public const FILE_EXTENSION_VUE = 'vue';
     final public const FILE_EXTENSION_SVG = 'svg';
     final public const FILE_EXTENSION_TXT = 'txt';
+    final public const FILE_EXTENSION_VUE = 'vue';
     final public const FILE_EXTENSION_XLSX = 'xlsx';
+    final public const FILE_EXTENSION_YML = 'yml';
 
     /**
      * Writes data to a file, creating the target directory recursively if it doesn't exist.
