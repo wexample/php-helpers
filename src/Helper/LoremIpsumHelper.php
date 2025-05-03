3906499cbdadc21b4ce5df277b9d87da8acd5397
weeger <romain.weeger@gmail.com>
2025-05-03 11:20:01 +0200

    Lorem ipsum

--- src/Helper/LoremIpsumHelper.php
+++ src/Helper/LoremIpsumHelper.php
@@ -8,14 +8,28 @@ namespace Wexample\Helpers\Helper;
 final class LoremIpsumHelper
 {
     /**
-     * Base Lorem Ipsum sentence
+     * Base Lorem Ipsum sentence that always starts the text
      */
-    private const BASE_LOREM_IPSUM =
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' .
-        'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+    private const BASE_START = 'Lorem ipsum';
+
+    /**
+     * Collection of Lorem Ipsum sentences to randomize
+     */
+    private const LOREM_SENTENCES = [
+        'dolor sit amet, consectetur adipiscing elit.',
+        'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.',
+        'deserunt mollit anim id est laborum.',
+        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.',
+        'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.',
+        'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.',
+    ];
 
     /**
      * Generate placeholder text of a given length (in characters).
+     * Text will start with "Lorem ipsum" and continue with random Lorem Ipsum sentences.
      *
      * @param int $length Length in characters. Must be >= 0.
      * @return string Lorem Ipsum text truncated or padded to length.
@@ -26,9 +40,16 @@ final class LoremIpsumHelper
             return '';
         }
 
-        $baseLength = mb_strlen(self::BASE_LOREM_IPSUM, 'UTF-8');
-        $repeats = (int) ceil($length / $baseLength);
-        $text = str_repeat(self::BASE_LOREM_IPSUM . ' ', $repeats);
+        // Always start with "Lorem ipsum"
+        $text = self::BASE_START . ' ';
+        $currentLength = mb_strlen($text, 'UTF-8');
+        
+        // Add random sentences until we reach desired length
+        while ($currentLength < $length) {
+            $sentence = self::LOREM_SENTENCES[array_rand(self::LOREM_SENTENCES)];
+            $text .= $sentence . ' ';
+            $currentLength = mb_strlen($text, 'UTF-8');
+        }
 
         return mb_substr(rtrim($text), 0, $length, 'UTF-8');
     }
