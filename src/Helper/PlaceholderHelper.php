2819fb59317a14a7b364470912563d867f4c1691
weeger <romain.weeger@gmail.com>
2026-07-26 10:01:34 +0200

    Cleanup

---
+++ src/Helper/PlaceholderHelper.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wexample\Helpers\Helper;
+
+class PlaceholderHelper
+{
+    public const DEMO_URL = 'http://wexample.com';
+
+    public const DEMO_EMAIL = 'example@wexample.com';
+}
